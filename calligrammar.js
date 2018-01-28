d30a33db00aa9e20574484005ef96d25d20948ca
jazz mickle <jaymickle@gmail.com>
2018-01-28 02:30:49 +0100

    Initial commit

---
+++ calligrammar.js
@@ -0,0 +1,10 @@
+
+window.onload = function() {
+
+var canvas = document.getElementById('maincanvas');
+var ctx = canvas.getContext('2d');
+ctx.fillStyle = 'white';
+ctx.font = "48px serif";
+ctx.strokeText("where the magic happens", 100, 100);
+
+};
